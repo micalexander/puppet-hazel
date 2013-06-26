7a748068c973ef651594b9ad3b41fad3e866789b
Andre Arko <andre@arko.net>
2013-06-26 09:52:04 -0700

    only run to install

--- manifests/init.pp
+++ manifests/init.pp
@@ -12,23 +12,26 @@ class hazel {
   exec { 'hazel-download':
     cwd => $install_dir,
     command => "curl -OLs https://s3.amazonaws.com/Noodlesoft/${dmg}",
-    creates => "${install_dir}/${dmg}"
+    creates => "${install_dir}/${dmg}",
+    subscribe => Exec['hazel-mount'],
+    refreshonly => true
   }
 
   exec { 'hazel-mount':
     cwd => $install_dir,
     command => "yes | hdiutil mount Hazel-3.0.16.dmg > /dev/null",
-    require => Exec['hazel-download']
+    subscribe => Exec['hazel-install'],
+    refreshonly => true
   }
 
   exec { 'hazel-install':
     command => "cp -R /Volumes/Hazel/${$pref_pane_name} ${pref_pane_dir}/",
     creates => "${pref_pane_dir}/${pref_pane_name}",
-    require => Exec["hazel-mount"],
+    notify => Exec['hazel-unmount']
   }
 
   exec { 'hazel-unmount':
-    command => "hdiutil unmount /Volumes/Hazel/"
+    command => "hdiutil unmount /Volumes/Hazel/",
+    refreshonly => true
   }
-  Exec['hazel-install'] -> Exec['hazel-unmount']
 }
